5033ee2a672d9f8c0cb1d99610413460982613e4
brandonaaron <brandonaaron@users.noreply.github.com>
2020-09-23 12:43:23 +0000

    Deploying to gh-pages from master @ 69565de93ecf9b9965d64156ef2a80d4bff8cce2 🚀

--- ssr-build/service-worker.js
+++
@@ -1,4 +0,0 @@
-importScripts("/precache-manifest.93080775e933fceed0feb920034c1a8d.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
-
